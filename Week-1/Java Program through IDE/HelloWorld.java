2862e3fd3dd6cba51c42baf444e51a70c122a963
nknantha <nknanthakumar13@gmail.com>
2021-07-23 21:35:48 +0530

    Added Java Program through IDE

--- Week-1/Java Program through IDE/HelloWorld.java
+++ Week-1/Java Program through IDE/HelloWorld.java
@@ -1,7 +1,20 @@
+/**
+ * A simple Hello World program in Java.
+ * 
+ * It contains a public class HelloWorld and a public
+ * static method main.
+ * While compile and run this file the main method inside HelloWorld
+ * will be automatically execute.
+ * 
+ * @author nknantha <https://github.com/nknantha>
+ */
+
+// Public class HelloWorld.
 public class HelloWorld {
     
+    // Public static method main.
     public static void main(String[] args) {
-        // Below line prints "Hello World"
+        // Below line prints "Hello World" in console.
         System.out.println("Hello World");
     }
 }
