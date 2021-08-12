cd5bf5291654fc060a8892e98f28fdc8f05a87dc
Sooram Nikitha <nikithaguptha6@gmail.com>
2021-08-12 17:29:37 +0530

    Added Classes and Objects topic

---
+++ Week-2/Classes and Objects/ClassesAndObjects.java
@@ -0,0 +1,9 @@
+// Create a Class
+//Main.java
+public class ClassesAndObjects {
+    int x=10;
+    public static void main(String[] args) {
+        ClassesAndObjects myObj = new ClassesAndObjects();
+        System.out.println(myObj.x);
+    }
+}
